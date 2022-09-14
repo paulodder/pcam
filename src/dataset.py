1536ad2098b74c1c16f372a5692e39321146b378
Pim Meerdink <pimmeerdink@wcw-stud-145-109-28-90.wireless.uva.nl>
2022-09-14 16:37:29 +0200

    rename dataloader/set

--- src/dataset.py
+++ src/dataset.py
@@ -10,7 +10,7 @@ class PcamDataset(Dataset):
     def __init__(self, x_path, y_path, meta_path):
         self.x = h5py.File(x_path, "r")["x"][:].squeeze()
         self.y = h5py.File(y_path, "r")["y"][:].squeeze()
-        self.meta = pd.read_csv(meta_path, "r")
+        self.meta = pd.read_csv(meta_path)
 
     def __len__(self):
         return self.x.shape[0]
