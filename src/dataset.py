a4fbd87522f2652978decc72e6759c2e6f962d75
Pim Meerdink <pimmeerdink@sp-byods-145-109-46-223.wireless.uva.nl>
2022-09-15 13:44:28 +0200

    add standardization

--- src/dataset.py
+++ src/dataset.py
@@ -4,11 +4,18 @@ from torch.utils.data import Dataset
 import h5py
 from pathlib import Path
 import pandas as pd
+from src.mean__std import get_mean__std
+
+from constants import DDIR, SPLIT_NAME2FNAME
 
 
 class PcamDataset(Dataset):
     def __init__(self, x_path, y_path, meta_path):
+        mean__std = get_mean__std()
+        self.mean = mean__std[0]
+        self.std = mean__std[1]
         self.x = h5py.File(x_path, "r")["x"][:].squeeze()
+
         self.y = h5py.File(y_path, "r")["y"][:].squeeze()
         self.meta = pd.read_csv(meta_path)
 
@@ -16,4 +23,20 @@ class PcamDataset(Dataset):
         return self.x.shape[0]
 
     def __getitem__(self, idx):
-        return Tensor(self.x[idx]), Tensor([self.y[idx]])
+        x = (self.x[idx] - self.mean) / self.std
+        y = [self.y[idx]]
+        return Tensor(x), Tensor(y)
+
+
+def get_dataset(split_name):
+    fpath_x, fpath_y, fpath_meta = (
+        DDIR / x for x in SPLIT_NAME2FNAME[split_name]
+    )
+    ds = PcamDataset(fpath_x, fpath_y, fpath_meta)
+    return ds
+
+
+def get_dataloader(split_name, batch_size=64):
+    ds = get_dataset(split_name)
+    dl = DataLoader(ds, batch_size=batch_size, shuffle=True)
+    return dl
---
+++ src/mean__std.py
@@ -0,0 +1,30 @@
+import h5py
+from pathlib import Path
+from decouple import config
+import numpy as np
+import pickle as pkl
+from src.constants import DDIR
+
+
+def calc_mean__std():
+    print("Calculating mean__std")
+    x = h5py.File(DDIR / "camelyonpatch_level_2_split_valid_x.h5", "r")["x"][
+        :
+    ].squeeze()
+    # x = h5py.File('camelyonpatch_level_2_split_train_x.h5', "r")["x"][:].squeeze()
+    mean = np.mean(x, axis=(0, 1, 2))
+    std = np.std(x, axis=(0, 1, 2))
+    mean__std = (mean, std)
+    return mean__std
+
+
+def get_mean__std():
+    path = Path(DDIR / "mean__std.pkl")
+    if path.exists():
+        with open(path, "rb") as f:
+            mean__std = pkl.load(f)
+    else:
+        mean__std = calc_mean__std()
+        with open(path, "wb") as f:
+            pkl.dump(mean__std, f)
+    return mean__std
