53c1d1d2fdee8c0f4f0f4ef3fda8014a283e38b5
Luc Weytingh <lucweytingh321@gmail.com>
2022-09-21 15:12:03 +0200

    works on windows now
    [Ticket: #X]

--- scripts/data2png.py
+++ scripts/data2png.py
@@ -3,6 +3,7 @@ from src.dataset import get_dataset
 from decouple import config
 from pathlib import Path
 import numpy as np
+import os
 
 img_folder = "imgs_png"
 Path(img_folder).mkdir(exist_ok=True)
@@ -12,4 +13,4 @@ ds = get_dataset("validation")
 for i, img_ten in enumerate(ds.x):
     img_ten = np.reshape(img_ten, (96, 96, 3))
     img = Image.fromarray(img_ten, "RGB")
-    img.save(config("PROJECT_DIR") + f"{img_folder}/img{i}.png")
+    img.save(os.path.join(config("PROJECT_DIR"), img_folder, "img{i}.png"))
