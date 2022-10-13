a7f36d0cca101598a468284bcb24ff178ff3c31e
Luc Weytingh <lucweytingh321@gmail.com>
2022-10-13 13:36:49 +0200

    pcam_predictor
    [Ticket: #X]

--- src/pcam_predictor.py
+++ src/pcam_predictor.py
@@ -3,6 +3,19 @@ import torch
 import torch.optim as optim
 import torch.nn as nn
 import pytorch_lightning as pl
+from torchmetrics import AUROC, Accuracy
+from torch.nn import functional as F
+import numpy as np
+
+from src.densenet import fA_P4DenseNet, fA_P4MDenseNet, P4MDenseNet, P4DenseNet
+
+NET_STR2INIT_FUNC = {
+    "fA_P4DenseNet": fA_P4DenseNet,
+    "fA_P4MDenseNet": fA_P4MDenseNet,
+    "P4MDenseNet": P4MDenseNet,
+    "P4DenseNet": P4DenseNet,
+    # "GResNet18": GResNet18,
+}
 
 
 class PCAMPredictor(pl.LightningModule):
@@ -31,6 +44,12 @@ class PCAMPredictor(pl.LightningModule):
             )
         self.loss_module = nn.BCEWithLogitsLoss()
         self.val_losses = []
+        self.auroc = {
+            "train": AUROC(num_classes=2, pos_label=1),
+            "val": AUROC(num_classes=2, pos_label=1),
+            "test": AUROC(num_classes=2, pos_label=1),
+        }
+        self.acc = {"train": Accuracy(), "val": Accuracy(), "test": Accuracy()}
 
     def configure_optimizers(self):
         optimizer = optim.AdamW(
@@ -45,15 +64,16 @@ class PCAMPredictor(pl.LightningModule):
         return optimizer
 
     def training_step(self, batch, batch_idx):
-        loss, acc = self.forward(batch, mode="train")
+        loss, acc, auc = self.forward(batch, mode="train")
         # wandb.log({"loss": loss, "acc": acc})
         return loss
 
     def validation_step(self, batch, batch_idx):
-        loss, acc = self.forward(batch, mode="val")
+        loss, acc, auc = self.forward(batch, mode="val")
         return {
             f"loss": loss,
             f"acc": acc,
+            f"auc": auc,
         }
 
     def validation_epoch_end(self, outputs):
@@ -64,10 +84,12 @@ class PCAMPredictor(pl.LightningModule):
             wandb_config["validate_on"], outputs
         ):
             acc = np.mean([tmp["acc"] for tmp in output])
+            auc = np.mean([tmp["auc"] for tmp in output])
             loss = np.mean([tmp["loss"].cpu() for tmp in output])
             wandb.log(
                 {
                     f"run{self.run_i}_{dataloader_name}_acc": acc,
+                    f"run{self.run_i}_{dataloader_name}_auc": acc,
                     f"run{self.run_i}_{dataloader_name}_loss": loss,
                 }
             )
@@ -76,28 +98,28 @@ class PCAMPredictor(pl.LightningModule):
                 self.val_losses.append(loss)
 
     def test_step(self, batch, batch_idx):
-        loss, acc = self.forward(batch, mode="test")
+        loss, acc, auc = self.forward(batch, mode="test")
 
         return {
             "loss": loss,
             "acc": acc,
+            "auc": auc,
         }
 
     def test_epoch_end(self, outputs):
         acc = np.mean([tmp["acc"] for tmp in outputs])
+        auc = np.mean([tmp["auc"] for tmp in outputs])
         loss = np.mean([tmp["loss"].cpu() for tmp in outputs])
-        # print(len(outputs), acc)
-        # wandb.log({"test_acc": acc, "test_loss": loss})
-        # self.log("test_acc", acc)
+        wandb.log({"test_acc": acc, "test_auc": auc, "test_loss": loss})
+        self.log("test_acc", acc)
 
     def forward(self, data, mode="train"):
         x, y = data
         outp = self.model(x)
         y_pred_proba = F.softmax(outp)
         loss = self.loss_module(y_pred_proba, y)
-        y_pred_proba = y_pred_proba.cpu().detach().numpy()
-        y = y.cpu().detach().numpy()
-        acc = sum(np.argmax(y_pred_proba, 1) == np.argmax(y, 1)) / len(
-            y_pred_proba
-        )
-        return loss, acc
+        y_pred_proba = y_pred_proba.cpu().detach()
+        y = y.cpu().detach().int()
+        auc = self.auroc[mode](y_pred_proba, y)
+        acc = self.acc[mode](y_pred_proba, y)
+        return loss, acc, auc
